77b0e7e73ac62db9a897f55c809c0beb90df9901
cchance6 <cchance6@gatech.edu>
2023-06-18 18:03:58 -0400

    Updated some styling

--- client/src/views/ContactPage.jsx
+++ client/src/views/ContactPage.jsx
@@ -1,8 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function ContactPage() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
   useEffect(() => {
     // Example starter JavaScript for disabling form submissions if there are invalid fields
     "use strict";
@@ -27,56 +33,86 @@ export default function ContactPage() {
     });
   }, []);
 
+  const handleChange = (evt) => {
+    setFormData({
+      ...formData,
+      [evt.target.name]: evt.target.value,
+    });
+  };
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+
+    let response = await fetch("http://localhost:8080/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(formData),
+    });
+    let result = await response.json();
+    alert(result);
+  };
+
   return (
-    <div className="row mt-5">
-      <h1 className="text-center">Contact Me</h1>
-      <div className="col-md-6 offset-md-3">
-        <form
-          action="/contact"
-          method="POST"
-          className="needs-validation"
-          noValidate
-        >
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">
-              Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              name="name"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label className="form-label" htmlFor="email">
-              Email
-            </label>
-            <input
-              className="form-control"
-              type="email"
-              id="email"
-              name="email"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="message" className="form-label">
-              Message
-            </label>
-            <textarea
-              className="form-control"
-              id="message"
-              rows="3"
-              name="message"
-              required
-            ></textarea>
-          </div>
-          <div className="mb-3 d-grid gap-2">
-            <button className="btn btn-success">Send Message</button>
-          </div>
-        </form>
+    <div className="container">
+      <div className="row mt-5">
+        <h1 className="text-center">Contact Me</h1>
+        <div className="col-md-6 offset-md-3">
+          <form onSubmit={handleSubmit} className="needs-validation" noValidate>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={(evt) => {
+                  handleChange(evt);
+                }}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="form-control"
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={(evt) => {
+                  handleChange(evt);
+                }}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="message" className="form-label">
+                Message
+              </label>
+              <textarea
+                className="form-control"
+                id="message"
+                rows="3"
+                name="message"
+                onChange={(evt) => {
+                  handleChange(evt);
+                }}
+                value={formData.message}
+                required
+              ></textarea>
+            </div>
+            <div className="mb-3 d-grid gap-2">
+              <button className="btn btn-success">Send Message</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
